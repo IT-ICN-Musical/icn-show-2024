c9768efa50fe819182beb3386084e3fb34b0a503
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-10-02 01:38:32 +0800

    adds cast selection menu/carousel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ['latin'] })
+const gyahegi = localFont({
+  src: "./Gyahegi.otf",
+  variable: "--font-gyahegi",
+})
+
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${montserrat.className} ${gyahegi.variable}`}>{children}</body>
     </html>
   )
 }
