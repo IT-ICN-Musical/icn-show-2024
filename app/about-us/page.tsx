ca27b758c6b39849eec4cd4eeae193e08d99eeba
BernardLesley <bernardlesleyefendy@gmail.com>
2023-12-13 21:25:49 +0800

    minor fix

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
 import React, { useState, useEffect } from "react";
-import AboutUsLogoDescription from "../components/AboutUsLogoDescription";
-import Tickets from "../components/Tickets";
+import AboutUsLogoDescription from "../(components)/AboutUsLogoDescription";
+import Tickets from "../(components)/Tickets";
 
 export default function aboutUsPage() {
-    return(
-        <div>
-            <AboutUsLogoDescription />
-            <Tickets />
-        </div>
-    )
+  return (
+    <div>
+      <AboutUsLogoDescription />
+      <Tickets />
+    </div>
+  );
 }
