5b218dc66011b66955b06483fdbbd61108d51759
Clayton Fernalo <clafernalo@gmail.com>
2023-12-14 20:40:14 +0700

    feat(landing): add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,74 @@
 "use client";
-
-import Navbar from "./(components)/Navbar";
-import Tickets from "./(components)/Tickets";
-import React, { useEffect } from "react";
-import Synopsis from "./(components)/Synopsis";
-import Loading from "./(components)/loading";
-import Footer from "./(components)/Footer";
+import Image from "next/image";
+import Synopsis from "./components/Synopsis";
+import Window from "./components/Window";
+import clsx from "clsx";
+import { useState } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <main>
-      <Loading interval={1500} />
-      <Navbar />
-      <Tickets />
+    <>
+      <button onClick={() => setIsOpen(!isOpen)}>Click me</button>
+      <div className="h-screen w-full bg-slate-500">
+        <Window>
+          <div className="bg-white w-screen h-screen">
+            <Image
+              src="/assets/landing-page/landing bg jendelalangit.png"
+              alt="test"
+              fill={true}
+              unoptimized
+              objectFit="cover"
+              className="scale-[1.4] translate-x-[1%] translate-y-[4%]"
+            />
+            <Image
+              src="/assets/landing-page/gedung.png"
+              alt="test"
+              fill={true}
+              unoptimized
+              objectFit="cover"
+              className="scale-[1.35] translate-y-[3%]"
+            />
+            <Image
+              src="/assets/landing-page/icn-logo.png"
+              alt="test"
+              width={0}
+              height={0}
+              unoptimized
+              objectFit="cover"
+              className={clsx(
+                "absolute left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4 w-3/6 duration-1000  ease-in-out transition-[transform_opacity]",
+                isOpen
+                  ? "scale-100 opacity-100 delay-[1500ms]"
+                  : "scale-50 opacity-0"
+              )}
+            />
+            <Image
+              src="/assets/landing-page/gorden kiri.png"
+              alt="test"
+              fill={true}
+              unoptimized
+              objectFit="cover"
+              className={clsx(
+                "scale-[1.4] translate-x-[1%] translate-y-[4%] transition-transform duration-1000 ease-in-out",
+                isOpen && "-translate-x-[35%]"
+              )}
+            />
+            <Image
+              src="/assets/landing-page/gorden kanan.png"
+              alt="test"
+              fill={true}
+              unoptimized
+              objectFit="cover"
+              className={clsx(
+                "scale-[1.4] translate-x-[1%] translate-y-[4%] transition-transform duration-1000 ease-in-out",
+                isOpen && "translate-x-[35%]"
+              )}
+            />
+          </div>
+        </Window>
+      </div>
       <Synopsis />
-      <Footer />
-    </main>
+    </>
   );
 }
