3f488a91ba98c0acabb554513005b311e2a14a48
BernardLesley <bernardlesleyefendy@gmail.com>
2023-12-18 21:04:35 +0800

    mobile responsive for new UI

--- app/store/(components)/ViewProductDialogContent.tsx
+++ app/store/(components)/ViewProductDialogContent.tsx
@@ -86,10 +86,10 @@ export default function ViewProductDialogContent({
   }, [getItemProducts, props.id, open]);
 
   useEffect(() => {
-    if (!open) {
+    if (!open || audienceNames.length === 0) {
       setIsEmptyInput(false);
     }
-  }, [open]);
+  }, [open, audienceNames]);
 
   const saveAndClose = () => {
     if (
@@ -124,9 +124,9 @@ export default function ViewProductDialogContent({
   };
 
   return (
-    <DialogContent className="p-0 max-w-none w-fit">
+    <DialogContent className="p-0 max-w-none min-[470px]:w-fit w-full">
       <div className="flex flex-col lg:flex-row px-5 my-5 lg:my-0">
-        <div className="mx-5 lg:mx-10 lg:my-10 flex lg:justify-center lg:flex-col w-[50vw] xl:w-[40vw] te">
+        <div className="mx-5 lg:mx-10 lg:my-10 flex lg:justify-center lg:flex-col w-full min-[470px]:w-[50vw] xl:w-[40vw] te">
           <Image
             className="object-cover rounded-xl w-[33rem] lg:block hidden"
             width={0}
@@ -139,41 +139,45 @@ export default function ViewProductDialogContent({
             }}
           />
           <div className="flex flex-col pt-2 lg:py-8 lg:px-1">
-            <Label className="font-bold text-3xl lg:text-4xl">
+            <Label className="font-bold text-2xl md:text-3xl lg:text-4xl lg:mb-0 mb-2">
               {props.name}
             </Label>
-            <Label className="my-0 text-base py-2">{props.description}</Label>
+            <Label className="my-0 text-base py-2 lg:block hidden">
+              {props.description}
+            </Label>
             <div className="flex flex-row items-center px-0.5">
               <FaCalendarDays size={15} />
-              <Label className="ml-2 my-0 text-base">{props.timing}</Label>
+              <Label className="ml-2 my-0 text-xs sm:text-sm lg:text-base">
+                {props.timing}
+              </Label>
             </div>
 
-            <Separator className="my-2" />
+            <Separator className="my-2 min-[470px]:w-auto w-11/12" />
 
-            <Label className="my-0 text-base">
+            <Label className="my-0 text-xs sm:text-sm lg:text-base">
               Stock Left: {props.stock}
-              <span className="mx-1 text-base lg:text-lg font-semibold">
+              <span className="mx-1 text-sm sm:text-base lg:text-lg font-semibold">
                 |
               </span>{" "}
               Quantity Ordered: {audienceNames.length}
             </Label>
-            <Label className="my-0 text-base py-1">
+            <Label className="my-0 text-xs sm:text-sm lg:text-base py-1">
               Sale Ends on: {formatDate(props.endPeriod)} SGT
             </Label>
-            <div className="text-xs my-1 italic">
+            <div className="text-[0.7rem] md:text-xs my-1 italic">
               *Kindly make sure to click{" "}
               <span className="font-bold">Save and Close</span> to save your
-              changes.
+              order.
             </div>
           </div>
         </div>
 
         <div className="flex flex-col px-5 w-full lg:w-3/5 h-full lg:py-10 py-0 justify-between">
-          <div className="flex flex-col justify-center max-h-[60vh] min-h-[15vh] lg:h-[60vh]">
-            <Label className="font-semibold sm:text-md lg:text-lg  mt-[20px] mb-[10px] px-2 text-center">
+          <div className="flex flex-col justify-center max-h-[30vh] sm:max-h-[45vh] min-h-[15vh] lg:h-[60vh] lg:max-h-[60vh]">
+            <Label className="font-semibold text-sm sm:text-base lg:text-lg lg:mt-[20px] mb-[10px] lg:px-2 text-center">
               Your Tickets for {props.name}
             </Label>
-            <ScrollArea className="max-h-[75%] pb-7">
+            <ScrollArea className="max-h-[75%] pb-2">
               {audienceNames.length > 0 ? (
                 <div className="w-11/12">
                   {audienceNames.map((audienceNameForProduct, productIndex) => (
@@ -182,7 +186,7 @@ export default function ViewProductDialogContent({
                       className="flex flex-col mb-5 justify-center text-center"
                     >
                       <div className="flex items-center ml-5 text-center">
-                        <Label className="font-cold text-xl my-2 mr-4">
+                        <Label className="font-cold text-base lg:text-lg my-1 lg:my-2 mr-2 lg:mr-4">
                           {`${
                             props.name.toLowerCase().includes("bundle")
                               ? "Bundle"
@@ -193,6 +197,7 @@ export default function ViewProductDialogContent({
                           variant="destructive"
                           onClick={() => handleDeleteProduct(productIndex)}
                           size="icon"
+                          className="w-8 h-8 lg:w-10 lg:h-10"
                         >
                           <i className="fa fa-trash" />
                         </Button>
@@ -202,7 +207,7 @@ export default function ViewProductDialogContent({
                         <Input
                           key={ticketIndex}
                           placeholder={`Enter Name`}
-                          className="w-full my-1 mx-2"
+                          className="h-8 lg:h-10 w-full my-1 mx-2"
                           value={
                             audienceNameForProduct[ticketIndex]
                               ? audienceNameForProduct[ticketIndex]
@@ -221,21 +226,23 @@ export default function ViewProductDialogContent({
                   ))}
                 </div>
               ) : (
-                <p className="text-center">
+                <p className="text-center text-xs sm:text-sm lg:text-base">
                   Click 'Add Tickets' to start adding tickets.
                 </p>
               )}
             </ScrollArea>
             {isEmptyInput && (
-              <p className="text-red-500 mb-1">
-                Warning: Please fill in all input boxes before closing.
-              </p>
+              <div className="flex items-center justify-center">
+                <p className="text-red-500 mb-2 text-xs sm:text-sm lg:text-base">
+                  Warning: Please fill in all input boxes before closing.
+                </p>
+              </div>
             )}
           </div>
 
           <div className="flex flex-end flex-col items-end justify-end">
             <Button
-              className={"rounded mx-2 my-2"}
+              className={"rounded mx-2 my-0.5 lg:my-2"}
               onClick={addProduct}
               variant="secondary"
               disabled={audienceNames.length >= props.stock}
